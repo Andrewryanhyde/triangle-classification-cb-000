411e106abe1ba93ed97a4f9d7a6242955d469246
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-29 08:32:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,32 @@ class Triangle
   attr_accessor :side_1, :side_2, :side_3
 
   def intitalize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
 
+  class TriangleError
 
   end
+
+  def kind
+    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+      raise TriangleError
+    end
+
+    if side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_1 + side_3 <= side_2
+      raise TriangleError
+    end
+
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif
+      side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
